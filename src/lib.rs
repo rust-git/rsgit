71734b718b9a9c9d7b4b4ae73e22a3acab716e31
Eric Scouten <eric@scouten.com>
2020-06-13 19:37:53 -0700

    Do-nothing function to test coverage.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
+pub fn do_nothing() {
+    println!("Nothing");
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn does_nothing() {
+        super::do_nothing();
     }
 }
