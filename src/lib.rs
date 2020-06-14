3eade02daa3bdfade0118e8855a19cc61d974ec4
Eric Scouten <eric@scouten.com>
2020-06-13 23:46:44 -0700

    Remove placeholder code in lib.rs. (#12)

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,2 @@
 mod attribution;
 pub use attribution::Attribution;
-
-pub fn do_nothing() {
-    println!("Nothing");
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn does_nothing() {
-        super::do_nothing();
-    }
-}
