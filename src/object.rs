c3eb0524a6557a4c8cb9173c4a684d946fd701a2
Eric Scouten <eric@scouten.com>
2020-06-15 20:51:04 -0700

    Add documentation.

--- src/object.rs
+++ src/object.rs
@@ -1,6 +1,7 @@
 //! Represents the git concept of an "object" which is a tuple of
 //! object type and binary data identified by the hash of the binary data.
 
+/// Describes the fundamental git object type (blob, tree, commit, or tag).
 pub enum ObjectType {
     Blob,
     Tree,
