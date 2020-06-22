06e3977f68876dd1132cadf59b314965eb225288
Eric Scouten <eric@scouten.com>
2020-06-21 20:59:28 -0700

    Add ObjectId struct and conversion to/from 40-char hex ID. (#22)
    This will become part of the `Object` struct and related APIs.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ pub use content_source::ContentSource;
 mod object;
 pub use object::Object;
 pub use object::ObjectKind;
+pub use object::ParseObjectIdError;
+pub use object::ParseObjectIdErrorKind;
 
 pub mod on_disk_repo;
 
