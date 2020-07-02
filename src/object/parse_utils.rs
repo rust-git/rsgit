ffeb41e4325e99a89a7e27bfd5471c0000593166
Eric Scouten <eric@scouten.com>
2020-07-01 20:47:38 -0700

    Implement parse_utils::read_line.

--- src/object/parse_utils.rs
+++ src/object/parse_utils.rs
@@ -1,3 +1,19 @@
+use std::io::{BufRead, Result};
+
+pub(crate) fn read_line(b: &mut dyn BufRead, line: &mut Vec<u8>) -> Result<usize> {
+    line.clear();
+
+    b.read_until(10, line)?;
+
+    if let Some(last) = line.last() {
+        if last == &10 {
+            line.truncate(line.len() - 1);
+        }
+    }
+
+    Ok(line.len())
+}
+
 pub(crate) fn header<'a>(line: &'a [u8], name: &[u8]) -> Option<&'a [u8]> {
     if line.contains(&b' ') {
         let (maybe_name, value) = split_once(line, &b' ');
@@ -20,7 +36,30 @@ pub(crate) fn split_once<'a>(s: &'a [u8], c: &u8) -> (&'a [u8], &'a [u8]) {
 
 #[cfg(test)]
 mod tests {
-    use super::header;
+    use super::*;
+
+    use std::io::Cursor;
+
+    #[test]
+    fn read_line() {
+        let mut c = Cursor::new(&b"abc\ndef\n");
+        let mut line: Vec<u8> = Vec::new();
+
+        assert_eq!(super::read_line(&mut c, &mut line).unwrap(), 3);
+        assert_eq!(line.as_slice(), b"abc");
+
+        let mut c = Cursor::new(&b"abc\n");
+        let mut line: Vec<u8> = Vec::new();
+
+        assert_eq!(super::read_line(&mut c, &mut line).unwrap(), 3);
+        assert_eq!(line.as_slice(), b"abc");
+
+        let mut c = Cursor::new(&b"abc");
+        let mut line: Vec<u8> = Vec::new();
+
+        assert_eq!(super::read_line(&mut c, &mut line).unwrap(), 3);
+        assert_eq!(line.as_slice(), b"abc");
+    }
 
     #[test]
     fn header_fn() {
