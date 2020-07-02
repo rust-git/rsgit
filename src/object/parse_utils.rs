3b5d8564af8d95a566e23d158f76c13353682dba
Eric Scouten <eric@scouten.com>
2020-07-01 20:13:22 -0700

    Implement parse_utils::header.

--- src/object/parse_utils.rs
+++ src/object/parse_utils.rs
@@ -1,6 +1,34 @@
+pub(crate) fn header<'a>(line: &'a [u8], name: &[u8]) -> Option<&'a [u8]> {
+    if line.contains(&b' ') {
+        let (maybe_name, value) = split_once(line, &b' ');
+        if maybe_name == name {
+            Some(value)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
 pub(crate) fn split_once<'a>(s: &'a [u8], c: &u8) -> (&'a [u8], &'a [u8]) {
     match s.iter().position(|b| b == c) {
         Some(n) => (&s[0..n], &s[n + 1..]),
         None => (s, &[]),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::header;
+
+    #[test]
+    fn header_fn() {
+        assert_eq!(header(b"tagger abc", b"tagger").unwrap(), b"abc");
+        assert_eq!(header(b"tagger ", b"tagger").unwrap(), b"");
+
+        assert_eq!(header(b"taggex abc", b"tagger"), None);
+        assert_eq!(header(b"tagger", b"tagger"), None);
+        assert_eq!(header(b"taggerx abc", b"tagger"), None);
     }
 }
