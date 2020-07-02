c0d32a748ae833d91f5a8f3e7c7af09010b1a645
Eric Scouten <eric@scouten.com>
2020-07-01 21:22:29 -0700

    Add parse_utils::object_id_is_valid().

--- src/object/parse_utils.rs
+++ src/object/parse_utils.rs
@@ -27,6 +27,22 @@ pub(crate) fn header<'a>(line: &'a [u8], name: &[u8]) -> Option<&'a [u8]> {
     }
 }
 
+pub(crate) fn object_id_is_valid(name: &[u8]) -> bool {
+    if name.len() == 40 {
+        name.iter().all(|&c| is_valid_hex_digit(c))
+    } else {
+        false
+    }
+}
+
+fn is_valid_hex_digit(c: u8) -> bool {
+    match c {
+        b'0'..=b'9' => true,
+        b'a'..=b'f' => true,
+        _ => false,
+    }
+}
+
 pub(crate) fn split_once<'a>(s: &'a [u8], c: &u8) -> (&'a [u8], &'a [u8]) {
     match s.iter().position(|b| b == c) {
         Some(n) => (&s[0..n], &s[n + 1..]),
@@ -69,5 +85,41 @@ mod tests {
         assert_eq!(header(b"taggex abc", b"tagger"), None);
         assert_eq!(header(b"tagger", b"tagger"), None);
         assert_eq!(header(b"taggerx abc", b"tagger"), None);
+    }
+
+    #[test]
+    fn object_id_is_valid_fn() {
+        assert_eq!(
+            object_id_is_valid(b"0123456789012345678901234567890123456789"),
+            true
+        );
+        assert_eq!(
+            object_id_is_valid(b"abcdef6789012345678901234567890123456789"),
+            true
+        );
+        assert_eq!(
+            object_id_is_valid(b"abcdefg789012345678901234567890123456789"),
+            false
+        );
+        assert_eq!(
+            object_id_is_valid(b"Abcdef6789012345678901234567890123456789"),
+            false
+        );
+        assert_eq!(
+            object_id_is_valid(b"0123456789/12345678901234567890123456789"),
+            false
+        );
+        assert_eq!(
+            object_id_is_valid(b"0123456789:12345678901234567890123456789"),
+            false
+        );
+        assert_eq!(
+            object_id_is_valid(b"012345678901234567890123456789012345678"),
+            false
+        );
+        assert_eq!(
+            object_id_is_valid(b"01234567890123456789012345678901234567890"),
+            false
+        );
     }
 }
