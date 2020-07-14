fd91600c18f1c9b8980f60f00649700406c9b180
Eric Scouten <eric@scouten.com>
2020-07-13 22:21:56 -0700

    I think I was chasing coverage on an unreachable case.
    Remove debugging output.

--- src/object/check_tree.rs
+++ src/object/check_tree.rs
@@ -46,7 +46,6 @@ pub fn tree_is_valid_with_platform_checks(
 
         if r.read_until(0, &mut this_line)? == 0 {
             // We've reached EOF: It's good.
-            // println!("OK, it's good");
             return Ok(true);
         }
 
@@ -65,14 +64,11 @@ pub fn tree_is_valid_with_platform_checks(
                     lc_path = lc_path.nfc().collect::<String>();
                 }
                 if lc_names.contains(&lc_path) {
-                    // println!("bailed @67");
                     return Ok(false);
                 }
                 lc_names.insert(lc_path);
             }
         }
-
-        // println!("ct @ 73");
 
         if !previous_line.is_empty() {
             let previous_line_slice = previous_line.as_slice();
@@ -84,12 +80,10 @@ pub fn tree_is_valid_with_platform_checks(
             // tricky.
 
             if this_path_mode.path == previous_path_mode.path {
-                // println!("bailed @ 85");
                 return Ok(false);
             }
 
             if this_path_mode.cmp(&previous_path_mode) != Ordering::Greater {
-                // println!("bailed @ 90");
                 return Ok(false);
             }
 
@@ -106,7 +100,6 @@ pub fn tree_is_valid_with_platform_checks(
                             break;
                         }
                         Ordering::Equal => {
-                            // println!("bailed @ 105");
                             return Ok(false);
                         }
                         Ordering::Greater => (),
@@ -114,24 +107,8 @@ pub fn tree_is_valid_with_platform_checks(
                 }
             }
 
-            // println!("loop @ 108");
-
-            // println!("@119 prev = {:?}", &previous_path_mode);
-            // println!("@119 this = {:?}", &this_path_mode);
-            // println!(
-            //     "@119  cmp = {:?}",
-            //     previous_path_mode.cmp_same_name(&this_path_mode)
-            // );
-
-            match previous_path_mode.cmp_same_name(&this_path_mode) {
-                Ordering::Less => (),
-                Ordering::Equal => {
-                    // println!("bailed @ 121");
-                    return Ok(false);
-                }
-                Ordering::Greater => {
-                    maybe_lingering_trees.push(previous_path_mode.path.to_owned());
-                }
+            if previous_path_mode.cmp_same_name(&this_path_mode) == Ordering::Greater {
+                maybe_lingering_trees.push(previous_path_mode.path.to_owned());
             }
         }
 
@@ -575,9 +552,6 @@ mod tests {
         cs.push_str(&entry("100644 A.e"));
         cs.push_str(&entry("40000 a"));
 
-        // assert_eq!(tree_is_valid(&cs).unwrap(), true);
-
-        // println!("test @ 563");
         assert_eq!(
             tree_is_valid_with_platform_checks(
                 &cs,
@@ -590,7 +564,6 @@ mod tests {
             false
         );
 
-        // println!("test @ 576");
         assert_eq!(
             tree_is_valid_with_platform_checks(
                 &cs,
