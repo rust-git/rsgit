009bdfda3c8cb79f32e97f36cb433396bc815f46
Eric Scouten <eric@scouten.com>
2020-07-19 15:05:04 -0700

    Fix formatting.

--- src/repo/mod.rs
+++ src/repo/mod.rs
@@ -11,12 +11,12 @@ pub mod on_disk;
 
 /// A struct that implements the `Repo` trait represents a particular mechanism
 /// for storing and accessing a git repo.
-/// 
+///
 /// The required methods on this trait represent the most primitive operations
 /// which must be defined for a given storage architecture. Consider the
 /// information stored in a typical `.git` directory in a local repository. You will
 /// be building an alternative to that storage mechanism.
-/// 
+///
 /// The provided methods on this trait represent the common "porcelain" and "plumbing"
 /// operations for a git repo, regardless of its storage mechanism.
 
