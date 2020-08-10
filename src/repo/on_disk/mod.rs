923e6f71c6b03a6a83de1cd3068ce3a1ded46495
Eric Scouten <eric@scouten.com>
2020-08-09 17:39:02 -0700

    Organize tests for repo::on_disk. (#68)
    Looks like this mod is going to get big so add scoping.

--- src/repo/on_disk/mod.rs
+++ src/repo/on_disk/mod.rs
@@ -143,74 +143,76 @@ fn create_refs_dir(git_dir: &Path) -> Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    mod new {
+        use super::super::*;
 
-    use std::fs;
+        use std::fs;
 
-    use crate::test_support::TempGitRepo;
+        use crate::test_support::TempGitRepo;
 
-    extern crate dir_diff;
-    extern crate tempfile;
+        extern crate dir_diff;
+        extern crate tempfile;
 
-    #[test]
-    fn new_happy_path() {
-        let tgr = TempGitRepo::new();
-        let work_dir = tgr.path();
-        let git_dir = work_dir.join(".git");
-        let r = OnDisk::new(&work_dir).unwrap();
-        assert_eq!(r.work_dir(), work_dir);
-        assert_eq!(r.git_dir(), git_dir.as_path());
-    }
-
-    #[test]
-    fn new_error_no_work_dir() {
-        let tgr = TempGitRepo::new();
-        let work_dir = tgr.path().join("bogus");
-        let err = OnDisk::new(&work_dir).unwrap_err();
-        if let Error::WorkDirDoesntExist(_) = err {
-            // expected
-        } else {
-            panic!("wrong error: {:?}", err);
+        #[test]
+        fn happy_path() {
+            let tgr = TempGitRepo::new();
+            let work_dir = tgr.path();
+            let git_dir = work_dir.join(".git");
+            let r = OnDisk::new(&work_dir).unwrap();
+            assert_eq!(r.work_dir(), work_dir);
+            assert_eq!(r.git_dir(), git_dir.as_path());
         }
-    }
 
-    #[test]
-    fn new_error_no_git_dir() {
-        let tempdir = tempfile::tempdir().unwrap();
-        let work_dir = tempdir.path();
-        let err = OnDisk::new(&work_dir).unwrap_err();
-        if let Error::GitDirDoesntExist(_) = err {
-            // expected
-        } else {
-            panic!("wrong error: {:?}", err);
+        #[test]
+        fn error_no_work_dir() {
+            let tgr = TempGitRepo::new();
+            let work_dir = tgr.path().join("bogus");
+            let err = OnDisk::new(&work_dir).unwrap_err();
+            if let Error::WorkDirDoesntExist(_) = err {
+                // expected
+            } else {
+                panic!("wrong error: {:?}", err);
+            }
         }
-    }
 
-    #[test]
-    fn matches_command_line_git() {
-        let tgr = TempGitRepo::new();
-        let c_path = tgr.path();
+        #[test]
+        fn error_no_git_dir() {
+            let tempdir = tempfile::tempdir().unwrap();
+            let work_dir = tempdir.path();
+            let err = OnDisk::new(&work_dir).unwrap_err();
+            if let Error::GitDirDoesntExist(_) = err {
+                // expected
+            } else {
+                panic!("wrong error: {:?}", err);
+            }
+        }
 
-        let r_path = tempfile::tempdir().unwrap();
-        OnDisk::init(r_path.path()).unwrap();
+        #[test]
+        fn matches_command_line_git() {
+            let tgr = TempGitRepo::new();
+            let c_path = tgr.path();
 
-        assert_eq!(
-            dir_diff::is_different(c_path, r_path.path()).unwrap(),
-            false
-        );
-    }
+            let r_path = tempfile::tempdir().unwrap();
+            OnDisk::init(r_path.path()).unwrap();
 
-    #[test]
-    fn err_if_git_dir_exists() {
-        let r_path = tempfile::tempdir().unwrap();
-        let git_dir = r_path.path().join(".git");
-        fs::create_dir_all(&git_dir).unwrap();
+            assert_eq!(
+                dir_diff::is_different(c_path, r_path.path()).unwrap(),
+                false
+            );
+        }
 
-        let err = OnDisk::init(r_path.path()).unwrap_err();
-        if let Error::GitDirShouldntExist(_) = err {
-            // expected case
-        } else {
-            panic!("wrong error: {:?}", err);
+        #[test]
+        fn err_if_git_dir_exists() {
+            let r_path = tempfile::tempdir().unwrap();
+            let git_dir = r_path.path().join(".git");
+            fs::create_dir_all(&git_dir).unwrap();
+
+            let err = OnDisk::init(r_path.path()).unwrap_err();
+            if let Error::GitDirShouldntExist(_) = err {
+                // expected case
+            } else {
+                panic!("wrong error: {:?}", err);
+            }
         }
     }
 }
