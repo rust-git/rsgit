2b6a35ba936e6422610db8d163b4de93678db82c
Eric Scouten <eric@scouten.com>
2020-09-20 22:23:58 -0700

    Add new test module for temporarily changing working directory.

--- src/test_support/mod.rs
+++ src/test_support/mod.rs
@@ -1,5 +1,12 @@
+// Items in this module (and submodules) are only used in test code,
+// so we disable the unused_imports warning.
+
+mod temp_cwd;
+
+#[allow(unused_imports)]
+pub(crate) use temp_cwd::TempCwd;
+
 mod temp_git_repo;
 
-// TempGitRepo is only used in test code. Override the unused warning.
 #[allow(unused_imports)]
 pub(crate) use temp_git_repo::TempGitRepo;
---
+++ src/test_support/temp_cwd.rs
@@ -0,0 +1,59 @@
+use std::env;
+use std::path::{Path, PathBuf};
+
+// A TempCwd allows you to temporarily change the current
+// working directory for the host process.
+//
+// When the struct goes out of scope, the current working
+// directory will be reset to its previous value.
+
+// Because this struct is intended for testing, its functions
+// panic instead of returning Result structs.
+
+// We override the dead_code warnings here because this
+// struct is only used in test code.
+#[allow(dead_code)]
+pub(crate) struct TempCwd {
+    old_path: PathBuf,
+}
+
+impl TempCwd {
+    // Temporarily change working directory. The existing working
+    // directory will be restored when the struct is dropped.
+    #[allow(dead_code)]
+    pub fn new<P: AsRef<Path>>(path: P) -> TempCwd {
+        let old_path = env::current_dir().unwrap();
+        env::set_current_dir(path).unwrap();
+
+        TempCwd { old_path }
+    }
+}
+
+impl Drop for TempCwd {
+    fn drop(&mut self) {
+        env::set_current_dir(&self.old_path).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::TempCwd;
+
+    use std::env;
+
+    #[test]
+    fn temp_cwd() {
+        let old_path = env::current_dir().unwrap();
+        let tempdir = tempfile::tempdir().unwrap();
+        let new_path = tempdir.path();
+
+        {
+            let _tcwd = TempCwd::new(new_path);
+            assert_ne!(env::current_dir().unwrap(), old_path);
+            // MacOS likes to rewrite the path to add a /private
+            // prefix, which makes it impossible to assert_eq!(..., new_path) here.
+        }
+
+        assert_eq!(env::current_dir().unwrap(), old_path);
+    }
+}
