9f047659f7fb280761f146a20a60a8502aa4491b
Eric Scouten <eric@scouten.com>
2020-10-11 14:39:09 -0700

    Support arbitrary object types beyond the officially-recognized four.
    Turns out you can create such by using `git hash-object --literally` so we need a way to represent those.

--- src/object/kind.rs
+++ src/object/kind.rs
@@ -1,13 +1,19 @@
 use std::fmt::{self, Display, Formatter};
 
 /// Describes the fundamental git object type (blob, tree, commit, or tag).
+///
 /// We use the word `kind` here to avoid conflict with the Rust reserved word `type`.
-#[derive(Copy, Clone, Debug, PartialEq)]
+///
+/// Though not widely supported or used, you can also store objects of other
+/// made-up types by using the `--literally` flag, so this type supports storing
+/// other values, albeit less efficiently than it does the built-in types.
+#[derive(Clone, Debug, PartialEq)]
 pub enum Kind {
     Blob,
     Tree,
     Commit,
     Tag,
+    Other(Vec<u8>),
 }
 
 impl Display for Kind {
@@ -17,6 +23,7 @@ impl Display for Kind {
             Kind::Tree => write!(f, "tree"),
             Kind::Commit => write!(f, "commit"),
             Kind::Tag => write!(f, "tag"),
+            Kind::Other(name) => write!(f, "{}", String::from_utf8_lossy(name)),
         }
     }
 }
@@ -38,5 +45,8 @@ mod tests {
 
         let k = Kind::Tag;
         assert_eq!(k.to_string(), "tag");
+
+        let k = Kind::Other(b"arbitrary".to_vec());
+        assert_eq!(k.to_string(), "arbitrary");
     }
 }
